c4c5568ed0c8f05a05c0a103b38affd239b9f8cd
korobokvk <korobokvk@gmail.com>
2017-09-29 01:55:45 +0300

    refactoring

--- src/app/components/main/module/main.component.ts
+++ src/app/components/main/module/main.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'main-comp',
-  templateUrl: '../customisation/main.component.html',
-  styleUrls: ['../customisation/css/main.component.css']
+  template: '<album-comp></album-comp>'
+  
 })
 export class MainComponent {
 
